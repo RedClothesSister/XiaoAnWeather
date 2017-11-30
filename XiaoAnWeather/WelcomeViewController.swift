0138209800942da7c954c305b07888ce036cf57d
RedClothesSister <1092592926@qq.com>
2017-11-30 10:58:58 +0800

    Add WelcomeViewController.swift File And Delete ViewController.swift File

---
+++ XiaoAnWeather/WelcomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeViewController.swift
+//  XiaoAnWeather
+//
+//  Created by 黄君伟 on 2017/11/30.
+//  Copyright © 2017年 黄君伟. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
